e73d4012aeb3cb324e14446214884d02dc245681
Ilya Maximov <xc.navi@gmail.com>
2022-02-11 16:37:00 +0200

    Ready solution vectors length + 2 constructors + triangles S + total sum of triangles s.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,54 @@
+import static java.lang.Math.round;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        int a1 = -3;
+        int a2 = 1;
+
+        int b1 = 1;
+        int b2 = 4;
+
+        int c1 = 4;
+        int c2 = -1;
+
+        int d1 = 1;
+        int d2 = -3;
+
+        int e1 = -3;
+        int e2 = -3;
+
+        Vector vs = new Vector();
+
+        Vector ab = new Vector(a1, a2, b1, b2);
+        Vector bc = new Vector(b1, b2, c1, c2);
+        Vector cd = new Vector(c1, c2, d1, d2);
+        Vector de = new Vector(d1, d2, e1, e2);
+        Vector ea = new Vector(e1, e2, a1, a2);
+
+        Vector bd = new Vector(b1, b2, d1, d2);
+        Vector ad = new Vector(a1, a2, d1, d2);
+
+        System.out.println("AB " + ab.len());
+        System.out.println("BC " + bc.len());
+        System.out.println("CD " + cd.len());
+        System.out.println("DE " + de.len());
+        System.out.println("EA " + ea.len());
+
+        System.out.println("BD " + bd.len());
+        System.out.println("AD " + ad.len());
+        double sbda = round(vs.heronS(bd.len(), ad.len(), ab.len()));
+        double sade = round(vs.heronS(ad.len(), de.len(), ea.len()));
+        double sbcd = vs.heronS(bc.len(), cd.len(), bd.len());
+
+        System.out.println("S b d a: " + sbda);
+        System.out.println("S a d e: " + sade);
+        System.out.println("S b c d: " + sbcd);
+        System.out.println("Total S: " + vs.totalS(sbda, sade, sbcd));
+
+    }
 
     /*
 
@@ -15,7 +65,6 @@ public class Main {
     EA = 4;
 
     BD = 7;
-
     AD = 5.66;
 
 S bda = 14;
--- src/Vector.java
+++ src/Vector.java
@@ -1,2 +1,38 @@
+import static java.lang.Math.*;
+
+
 public class Vector {
+    private int a1;
+    private int a2;
+    private int b1;
+    private int b2;
+
+
+    public Vector(int a1, int a2, int b1, int b2) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.b1 = b1;
+        this.b2 = b2;
+    }
+
+    public Vector() {
+    }
+
+
+    public double len() {
+        return sqrt(pow(this.b1 - this.a1, 2) + pow(this.b2 - this.a2, 2));
+    }
+
+    public double heronS(double a, double b, double c) {
+
+        double p = (a + b + c) / 2;
+        return sqrt(p * (p - a) * (p - b) * (p - c));
+
+    }
+
+    public double totalS(double s1, double s2, double s3) {
+
+        return s1 + s2 + s3;
+    }
+
 }
