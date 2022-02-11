9bdfa13c20cce036ee4f51e7a4ec8fa0805dc6a2
Ilya Maximov <xc.navi@gmail.com>
2022-02-11 15:53:21 +0200

    Main+Vector

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
 public class Main {
 
-    
+    /*
+
+    a = -3,1;
+    b = 1,4;
+    c = 4,-1
+    d = 1, -3;
+    e = -3, -3
+    S = 32.5;
+    AB = 5;
+    BC = 5.83;
+    CD = 3.61;
+    DE = 4;
+    EA = 4;
+
+    BD = 7;
+
+    AD = 5.66;
+
+S bda = 14;
+S ade = 8;
+S bcd = 10.5;
+
+S = S(bda) + S(ade) + S(bcd) = 32.5
+     */
 }
